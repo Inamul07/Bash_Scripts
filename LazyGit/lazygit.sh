69a9e352d5d0b3c95a51715039fca77fc9164949
Inamul07 <mohammedinamul87@gmail.com>
2024-06-26 16:29:06 +0530

    Update: LazyGit

--- LazyGit/lazygit.sh
+++ LazyGit/lazygit.sh
@@ -1,4 +1,5 @@
-git -C ~/Desktop/scripts/ add ~/Desktop/scripts/
+repo=~/Desktop/scripts/
+git -C $repo add ~/Desktop/scripts/
 read -p "Enter Commit Message: " msg
-git -C ~/Desktop/scripts/ commit -m "$msg"
-git -C ~/Desktop/scripts/ push -u origin main
+git -C $repo commit -m "$msg"
+git -C $repo push -u origin main
